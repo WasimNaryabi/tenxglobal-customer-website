4b8369ea4fa9c14c638e34451c1c38295acba9a1
wasim ahmad <waseemnaryabi2018@gmail.com>
2026-01-03 02:34:51 +0500

    code

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,23 +1,25 @@
 import './bootstrap';
+import '../css/app.css';  // Import CSS here
 import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createPinia } from 'pinia';
 
 const pinia = createPinia();
 
 createInertiaApp({
-    resolve: name => {
-        const pages = import.meta.glob('./Pages/**/*.vue', { eager: true });
-        return pages[`./Pages/${name}.vue`];
-    },
+    title: (title) => title ? `${title} - ${import.meta.env.VITE_APP_NAME || 'Laravel'}` : import.meta.env.VITE_APP_NAME || 'Laravel',
+    resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
     setup({ el, App, props, plugin }) {
-        createApp({ render: () => h(App, props) })
+        return createApp({ render: () => h(App, props) })
             .use(plugin)
             .use(pinia)
             .mount(el);
     },
     progress: {
+        delay: 250,
         color: '#dc2626',
+        includeCSS: true,
         showSpinner: true,
     },
 });
